0a8204bc04cc0dfbb192d07a957465c94ccee9d2
vlados <vladik.mc777@gmail.com>
2026-09-10 14:29:06 +0300

    proverka

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,4 +1,9 @@
 void main(){
+<<<<<<< HEAD
+=======
+  print(greet('qwe'));
+  print(greet('zxc'));
+>>>>>>> 3d00503 (proverka)
 String name = 'vlad';
 int age = 24;
 double height = 3.33;
@@ -34,6 +39,7 @@ print('дарова $name tebe $age let');
 print(age);
 print(height);
 print(isSyudent);
+<<<<<<< HEAD
 
 
 
@@ -43,6 +49,8 @@ String greet(String name){
 }
 
 
+=======
+>>>>>>> 3d00503 (proverka)
 int sqare(int x)=> x*x;
 
 void Pet({required String name, String species = 'cot', int age =0}){
@@ -55,6 +63,17 @@ Pet(name: 'barsik',species: 'psina');
 List<int> numbers = [3,2,5,1,2,3,5];
 numbers.sort((a,b)=> b-a);
 print(numbers);
+<<<<<<< HEAD
 
 
 }
+=======
+}
+
+String greet(String name){
+  return 'darova, $name';
+  String greet(String name)=> 'hello, $name';
+  int square(int x)=>x*x;
+  double half(double x)=>x/2;
+}
+>>>>>>> 3d00503 (proverka)
